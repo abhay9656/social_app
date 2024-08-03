d39623bd8081a9cb4d1434eab1bf1c26e2928e7b
abhay9656 <abhay9656gupta@gmail.com>
2024-08-03 15:35:03 +0530

    Update Feed screen to include form for posting

--- Screens/Feed.jsx
+++ Screens/Feed.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import {Text, View } from 'react-native'
+import { TextInput,Button } from 'react-native-paper'
 
 const Feed = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
   return (
     <View>
       <Text>
          Feed Screen
-         
+
       </Text>
+      <TextInput label={'Post Title'}></TextInput>
+      <TextInput label={'Post Description'}></TextInput>
+        <Button mode={'contained'}>Post</Button>
     </View>
   )
 }
