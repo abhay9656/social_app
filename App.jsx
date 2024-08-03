165ed6535b5200674eda7621d7f89335a6354203
abhay9656 <abhay9656gupta@gmail.com>
2024-08-03 15:19:43 +0530

    Update dependencies and remove unused code in App.js

--- App.jsx
+++ App.jsx
@@ -1,12 +1,15 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Tab= createBottomTabNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <Tab.Navigator>
+     <Tab.Screen name="Home" component={Home}/>
+     <Tab.Screen name="Profile" component={Profile}/>
+     </Tab.Navigator>
   );
 }
 
---
+++ Screens/Feed.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Feed = () => {
+  return (
+    <View>
+      <Text>
+         Add 
+      </Text>
+    </View>
+  )
+}
+
+export default Feed
