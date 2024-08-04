bf5a0b9252dcd85a7eeee1afa14a0ffb540f2aed
abhay9656 <abhay9656gupta@gmail.com>
2024-08-04 15:18:41 +0530

    Integrate react-hook-form in Feed.jsx and update dependencies

--- Screens/Feed.jsx
+++ Screens/Feed.jsx
@@ -3,12 +3,25 @@ import {Text, View } from 'react-native'
 import { TextInput,Button } from 'react-native-paper'
 import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import app from '../firebaseConfig';
+import { useForm } from 'react-hook-form';
 
 const db =getFirestore(app);
 
 const Feed = () => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
+
+    const {
+      control,
+      handleSubmit,
+      formState: { errors },
+    } = useForm({
+      defaultValues: {
+        title: "",
+        description: "",
+      },
+    })
+    const onSubmit = (data) => console.log(data)
 
     const publishPost = async () =>{
       const docRef=await addDoc(collection(db,'socialposts'),{title,description})
