a04a8995c9f9524810db5380cc1cac9b87c1ea98
abhay9656 <abhay9656gupta@gmail.com>
2024-08-04 15:08:57 +0530

    Update margin top in App.jsx and integrate Firebase in Feed.jsx

--- Screens/Feed.jsx
+++ Screens/Feed.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {Text, View } from 'react-native'
 import { TextInput,Button } from 'react-native-paper'
-import { getFirestore } from 'firebase/firestore';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import app from '../firebaseConfig';
 
 const db =getFirestore(app);
@@ -10,15 +10,12 @@ const Feed = () => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
 
-    const publishPost = async()=>{
+    const publishPost = async () =>{
       const docRef=await addDoc(collection(db,'socialposts'),{title,description})
       console.log(docRef);
     }
   return (
     <View>
-      <Text>
-         Feed Screen
-      </Text>
       <TextInput onChangeText={setTitle} label={'Post Title'}></TextInput>
       <TextInput onChangeText={setDescription} label={'Post Description'}></TextInput>
         <Button onPress={publishPost} mode={'contained'}>Post</Button>
