1485f3f4ef4cf22efb87591869c1862bd5cd2bd5
abhay9656 <abhay9656gupta@gmail.com>
2024-08-04 16:08:14 +0530

    Integrate react-hook-form in Feed.jsx, update dependencies, and add expo-image-picker

--- Screens/Feed.jsx
+++ Screens/Feed.jsx
@@ -4,12 +4,30 @@ import { TextInput,Button } from 'react-native-paper'
 import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import app from '../firebaseConfig';
 import { Controller, useForm } from 'react-hook-form';
+import * as ImagePicker from 'expo-image-picker';
 
 const db =getFirestore(app);
 
 const Feed = () => {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
+    // const [title, setTitle] = useState('');
+    // const [description, setDescription] = useState('');
+
+    const [image, setImage] = useState(null)
+
+    const pickImage = async () => {
+      // No permissions request is necessary for launching the image library
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      console.log(result);
+
+      if (!result.canceled) {
+        setImage(result.assets[0].uri);
+      }
+    };
 
     const {
       control,
@@ -33,6 +51,8 @@ const Feed = () => {
   return (
     <View>
       <Text style={{textAlign:'center',fontSize:24}}>Add New Post</Text>
+      <Button onPress={pickImage} >Pick an image</Button>
+      {image && <Image source={{ uri: image }} resizeMode='contain' style={{width:'100%',height:300}} />}
       <Controller
         control={control}
         rules={{
@@ -67,6 +87,8 @@ const Feed = () => {
         name="description"
       />
       <Text>{errors.title?.message}</Text>
+
+      
       
       
         <Button onPress={handleSubmit(onSubmit)} mode={'contained'}>Post</Button>
