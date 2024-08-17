f45287d07b89a0487fc7d05fb47bdf623726b0d2
abhay9656 <abhay9656gupta@gmail.com>
2024-08-17 13:36:04 +0530

    Refactor Feed.jsx, integrate react-hook-form, update dependencies, and add expo-image-picker

--- Screens/Feed.jsx
+++ Screens/Feed.jsx
@@ -1,8 +1,17 @@
+import { getFirestore } from 'firebase/firestore';
 import React from 'react'
 import { View } from 'react-native'
 import { Text } from 'react-native-paper'
+import app from '../firebaseConfig';
+
+const db= getFirestore(app);
 
 const Feed = () => {
+
+  const loadFeed = async () => {
+    // Fetch feed from
+  }
+
   return (
     <View>
       <Text>Feed</Text>
