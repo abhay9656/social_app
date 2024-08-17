cd6bd693e8f23da1daf8f46fa351e0189bfa7942
abhay9656 <abhay9656gupta@gmail.com>
2024-08-17 14:18:33 +0530

    Refactor Feed.jsx, update likePost function to increment likes count

--- Screens/Feed.jsx
+++ Screens/Feed.jsx
@@ -7,12 +7,22 @@ import app from '../firebaseConfig';
 const db= getFirestore(app);
 
 const FeedCard =({data,index})=>{
+
+  const likePost = async()=>{
+    const ref =collection(db,'socialpost');
+    await ref.doc(data.id).update({
+      likes:data.likes+1
+    })
+  }
   return <Card style={styles.Card} key={data.id}>
    <Card.Title title={data.title} subtitle={data.description}/>
    <Text>{new Date(data.postedOn).toDateString()}</Text>
    <Card.Cover source={{uri:data.image}}/>
    <View style={styles.container}>
+     <View style={styles.iconButton}>
       <IconButton icon="heart-outline" mode='contained'/>
+      <Text>{data.likes ? data.likes:0}</Text>
+      </View> 
       <IconButton icon="comment-outline" mode='contained'/>
       <IconButton icon="share" mode='contained'/>
    </View>
@@ -70,6 +80,12 @@ const styles = StyleSheet.create({
   },
   Card:{
     margin:10
+  },
+  iconButton:{
+    flexDirection:'row',
+    justifyContent:'space-around',
+    gap:10,
+    alignItems:'center'
   }
 })
 export default Feed
