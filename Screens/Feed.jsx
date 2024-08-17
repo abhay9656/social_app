ca53e4b592a7a06d394350709d95abe06b360a69
abhay9656 <abhay9656gupta@gmail.com>
2024-08-17 14:13:52 +0530

    Refactor Feed.jsx, update console.log statement, integrate react-hook-form, and optimize code

--- Screens/Feed.jsx
+++ Screens/Feed.jsx
@@ -1,32 +1,75 @@
 import { collection, getDocs, getFirestore } from 'firebase/firestore';
-import React, { useEffect } from 'react'
-import { View } from 'react-native'
-import { Text } from 'react-native-paper'
+import React, { useEffect, useState } from 'react'
+import { FlatList,StyleSheet, View } from 'react-native'
+import { Card, IconButton, Text } from 'react-native-paper'
 import app from '../firebaseConfig';
 
 const db= getFirestore(app);
 
+const FeedCard =({data,index})=>{
+  return <Card style={styles.Card} key={data.id}>
+   <Card.Title title={data.title} subtitle={data.description}/>
+   <Text>{new Date(data.postedOn).toDateString()}</Text>
+   <Card.Cover source={{uri:data.image}}/>
+   <View style={styles.container}>
+      <IconButton icon="heart-outline" mode='contained'/>
+      <IconButton icon="comment-outline" mode='contained'/>
+      <IconButton icon="share" mode='contained'/>
+   </View>
+  </Card>
+}
+
 const Feed = () => {
 
+  const [feedList, setfeedList] = useState([])
+  const [refreshing, setrefreshing] = useState(false)
   const loadFeed = async () => {
     // Fetch feed from
     const ref=collection(db,'socialpost');
+    setrefreshing(true);
     const snapshot =await getDocs(ref);
     const data = snapshot.docs.map(doc =>{
       return {...doc.data(),id:doc.id}
     })
     console.log(data);
+    setfeedList(data);
+    setrefreshing(false);
   }
 
   useEffect(() => {
     loadFeed();
   },[]);
 
+
+  const displayFeed =()=>{
+    return <View>
+      <FlatList
+      data={feedList}
+      renderItem={({item,index})=> <FeedCard data={item} index={index} />} 
+      refreshing={refreshing} onRefresh={loadFeed}/> 
+    </View>
+  }
+
   return (
     <View>
-      <Text>Feed</Text>
+      <Text style={{fontSize:24,textAlign:'center'}}>Feed</Text>
+      {displayFeed()}
     </View>
   )
+ 
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flexDirection:'row',
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:10
+  },
+  Card:{
+    margin:10
+  }
+})
 export default Feed
