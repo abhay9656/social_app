069aa8d7a1003c3ea97b438831924d81f195d4bf
abhay9656 <abhay9656gupta@gmail.com>
2024-09-07 13:00:04 +0530

    Refactor App.jsx, add login and signup functionality, and update navigation options

--- App.jsx
+++ App.jsx
@@ -4,16 +4,60 @@ import { StyleSheet, Text, View } from 'react-native';
 import AddPost from './Screens/AddPost';
 import Feed from './Screens/Feed';
 import { NavigationContainer } from '@react-navigation/native';
-const Tab= createBottomTabNavigator();
+import { IconButton } from 'react-native-paper';
+import Signup from './Screens/Signup';
+import { useEffect, useState } from 'react';
+import Login from './Screens/Login';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import app from './firebaseConfig';
+
+const auth = getAuth(app);
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+  const [signupOpen, setSignupOpen] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserLoggedIn(true);
+      } else {
+        setUserLoggedIn(false);
+      }
+    });
+  }, [])
+
+
+  const openLogin = () => {
+    setLoginOpen(true);
+    setSignupOpen(false);
+  }
+
+  const openSignup = () => {
+    setSignupOpen(true);
+    setLoginOpen(false);
+  }
+
   return (
-   < NavigationContainer>
-   <Tab.Navigator>
-     <Tab.Screen name="Feed" component={Feed}/>
-     <Tab.Screen name="AddPost" component={AddPost}/>
-     </Tab.Navigator>
-     </NavigationContainer>
+    <NavigationContainer>
+      <Signup visible={signupOpen} setVisible={setSignupOpen} openLogin={openLogin} />
+      <Login visible={loginOpen} setVisible={setLoginOpen} openSignup={openSignup} />
+      <Tab.Navigator>
+        <Tab.Screen name="Feed" component={Feed} options={{
+          headerRight: () => !userLoggedIn ? <IconButton
+            icon={'account-circle'}
+            onPress={() => { setLoginOpen(true); }} />
+            :
+            <IconButton
+              icon={'logout'}
+              onPress={() => { setUserLoggedIn(false); }} />
+        }} />
+        <Tab.Screen name="Addpost" component={AddPost} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -23,6 +67,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop:10
   },
 });
--- Screens/Feed.jsx
+++ Screens/Feed.jsx
@@ -1,91 +1,132 @@
-import { collection, getDocs, getFirestore } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react'
-import { FlatList,StyleSheet, View } from 'react-native'
-import { Card, IconButton, Text } from 'react-native-paper'
+
+import { collection, doc, getDoc, getDocs, getFirestore, setDoc } from 'firebase/firestore';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import app from '../firebaseConfig';
+import { Button, Card, IconButton } from 'react-native-paper';
+import Comments from './Comments';
+// import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 
-const db= getFirestore(app);
+const db = getFirestore(app);
 
-const FeedCard =({data,index})=>{
+const FeedCard = ({ data, feedList, setFeedList, index }) => {
 
-  const likePost = async()=>{
-    const ref =collection(db,'socialpost');
-    await ref.doc(data.id).update({
-      likes:data.likes+1
-    })
+  const [showComments, setShowComments] = useState(false);
+
+  const likePost = async () => {
+
+    const ref = doc(db, 'socialpost', data.id);
+    await setDoc(ref,
+      { likes: (data.likes ? data.likes : 0) + 1 },
+      { merge: true });
+
+    const updateData = (await getDoc(ref)).data();
+
+    const temp = feedList;
+    temp[index] = { ...data, likes: updateData.likes };
+    setFeedList([...temp]);
+
   }
-  return <Card style={styles.Card} key={data.id}>
-   <Card.Title title={data.title} subtitle={data.description}/>
-   <Text>{new Date(data.postedOn).toDateString()}</Text>
-   <Card.Cover source={{uri:data.image}}/>
-   <View style={styles.container}>
-     <View style={styles.iconButton}>
-      <IconButton icon="heart-outline" mode='contained'/>
-      <Text>{data.likes ? data.likes:0}</Text>
-      </View> 
-      <IconButton icon="comment-outline" mode='contained'/>
-      <IconButton icon="share" mode='contained'/>
-   </View>
-  </Card>
+
+  return <>
+    <Card key={data.id} style={styles.card}>
+      <Card.Title title={data.title} subtitle={data.description} />
+      <Text>{new Date(data.postedOn).toDateString()}</Text>
+      <Card.Cover source={{ uri: data.image }} />
+      <View style={styles.iconContainer}>
+        <View style={styles.iconButton}>
+          <IconButton
+            onPress={likePost}
+            icon="heart-outline"
+            mode="contained" />
+          <Text>{data.likes ? data.likes : "0"}</Text>
+        </View>
+        <View style={styles.iconButton}>
+          <IconButton
+            onPress={() => setShowComments(true)}
+            icon="comment-outline"
+            mode="contained" />
+            <Text>{data.comments ? data.comments.length : "0"}</Text>
+        </View>
+        <IconButton
+          icon="share"
+          mode="contained" />
+      </View>
+    </Card>
+    <Comments
+      feedList={feedList}
+      setFeedList={setFeedList}
+      index={index}
+      visible={showComments}
+      setVisible={setShowComments}
+      postData={data} />
+  </>
 }
 
 const Feed = () => {
 
-  const [feedList, setfeedList] = useState([])
-  const [refreshing, setrefreshing] = useState(false)
+  const [feedList, setFeedList] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const bottomSheetRef = useRef(null);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
   const loadFeed = async () => {
-    // Fetch feed from
-    const ref=collection(db,'socialpost');
-    setrefreshing(true);
-    const snapshot =await getDocs(ref);
-    const data = snapshot.docs.map(doc =>{
-      return {...doc.data(),id:doc.id}
-    })
-    console.log(data);
-    setfeedList(data);
-    setrefreshing(false);
+    const ref = collection(db, 'socialpost');
+    setRefreshing(true);
+    const snapshot = await getDocs(ref);
+    const data = snapshot.docs.map(doc => {
+      return { ...doc.data(), id: doc.id }
+    });
+    // console.log(data);
+    setFeedList(data);
+    setRefreshing(false);
   }
 
   useEffect(() => {
     loadFeed();
-  },[]);
+  }, []);
 
-
-  const displayFeed =()=>{
+  const displayFeed = () => {
     return <View>
       <FlatList
-      data={feedList}
-      renderItem={({item,index})=> <FeedCard data={item} index={index} />} 
-      refreshing={refreshing} onRefresh={loadFeed}/> 
+        data={feedList}
+        renderItem={({ item, index }) => <FeedCard data={item} feedList={feedList} setFeedList={setFeedList} index={index} />}
+        keyExtractor={(item) => item.id}
+        onRefresh={loadFeed}
+        refreshing={refreshing}
+      />
     </View>
   }
 
   return (
-    <View>
-      <Text style={{fontSize:24,textAlign:'center'}}>Feed</Text>
+    <View style={styles.container}>
+      <Text>Feed Page</Text>
       {displayFeed()}
+
+
     </View>
   )
- 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection:'row',
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop:10
+  card: {
+    margin: 10
   },
-  Card:{
-    margin:10
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10
   },
-  iconButton:{
-    flexDirection:'row',
-    justifyContent:'space-around',
-    gap:10,
-    alignItems:'center'
+  iconButton: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center'
   }
 })
-export default Feed
+
+export default Feed;
