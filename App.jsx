58232981e697994597c632ddc8b182e29f250d47
abhay9656 <abhay9656gupta@gmail.com>
2024-09-07 13:33:23 +0530

    Refactor App.jsx to include custom tab icons and update theme colors

--- App.jsx
+++ App.jsx
@@ -4,17 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import AddPost from './Screens/AddPost';
 import Feed from './Screens/Feed';
 import { NavigationContainer } from '@react-navigation/native';
-import { IconButton } from 'react-native-paper';
+import { IconButton, useTheme } from 'react-native-paper';
 import Signup from './Screens/Signup';
 import { useEffect, useState } from 'react';
 import Login from './Screens/Login';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import app from './firebaseConfig';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const auth = getAuth(app);
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+  const theme=useTheme();
 
   const [signupOpen, setSignupOpen] = useState(false);
   const [loginOpen, setLoginOpen] = useState(false);
@@ -54,7 +57,8 @@ export default function App() {
             }else if(route.name === 'Addpost'){
               iconName = focused ? 'plus-circle' : 'plus-circle-outline';
             }
-            return <IconButton icon={iconName} size={size} color={focused ?'blue':'black'} />
+            return <Icon name={iconName} size={focused ? 30 :25} color={focused ?theme.colors.primary:theme.colors.onPrimaryContainer
+            } />
           },
           tabBarActiveTintColor: 'blue',
           tabBarInactiveTintColor: 'gray',
